1b07877dae8fcc802410fbb1031f7c32bff36fc2
sunyiteng <sunyiteng@bytedance.com>
2023-04-27 21:09:35 +0800

    chore: add more detail about error

--- 18-error/apps/react17/index.tsx
+++ 18-error/apps/react17/index.tsx
@@ -1,15 +1,21 @@
 import { Route } from 'react-router';
 import React from 'react';
-// I can`t use "useDeferredValue"
+import { Helmet } from 'react-helmet';
 
 import ReactDom from 'react-dom';
 
+// I can`t use "useDeferredValue"
 const domNode = document.getElementById('root')!;
 
+const node: React.ReactNode = {};
+
 const x = () => {
-  return <Route>
-    <div></div>
-  </Route>;
+  return (
+    <Route>
+      <Helmet />
+      <div></div>
+    </Route>
+  );
 };
 
 ReactDom.render(x(), domNode);
--- 18-error/apps/react18/index.tsx
+++ 18-error/apps/react18/index.tsx
@@ -1,15 +1,24 @@
 import { Route } from 'react-router';
-import React, {useDeferredValue} from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Helmet } from 'react-helmet';
+
+import ReactDom from 'react-dom';
 
 const domNode = document.getElementById('root')!;
-const root = createRoot(domNode);
+
+// @ts-expect-error Type "{}" cannot be assigned to type "ReactNode". (@types/react 18.0.x)
+const node: React.ReactNode = {}; 
 
 const x = () => {
   return (
     <Route>
+      <Helmet />
       <div></div>
     </Route>
   );
 };
+
+
+const root = createRoot(domNode);
 root.render(x());
